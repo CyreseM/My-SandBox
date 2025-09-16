f679c995b885e22b1b966386643e176299867b2d
Cyrese Mills <cyrese.mills@persol.net>
2025-09-16 13:18:52 +0000

    Fixed video view

--- StatusTest/StatusApi/StatusAPI/obj/Debug/net9.0/StatusAPI.AssemblyInfo.cs
+++ StatusTest/StatusApi/StatusAPI/obj/Debug/net9.0/StatusAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StatusAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d4aa9bbbbedbd864a4ca9c973f4ab454f5aa65c")]
 [assembly: System.Reflection.AssemblyProductAttribute("StatusAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StatusAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
